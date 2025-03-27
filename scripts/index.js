33a2fda0e6d2712b12e9ee5f29c9cfb05bbde439
MrTheSumraak <maew202139@gmail.com>
2025-03-27 10:08:52 +0300

    добавление скрипта в проект

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,37 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+import initialCards from './cards.js';
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+const cardCreate = (cards) => {
+  const cardClone = cardTemplate.cloneNode(true);
+
+  const cardImage = cardClone.querySelector('.card__image');
+  const cardTitle = cardClone.querySelector('.card__title');
+  const cardDelete = cardClone.querySelector('.card__delete-button');
+
+  cardImage.src = cards.link;
+  cardTitle.innerText = cards.name;
+
+  cardDelete.addEventListener('click', () => {
+    const cardElement = cardDelete.closest('.card');
+    if (cardElement) {
+      cardElement.remove();
+    }
+  });
+
+  return cardClone;
+};
+
+const cardLoad = (cards) => {
+  cards.forEach((card) => {
+    const cardElement = cardCreate(card);
+    placesList.appendChild(cardElement);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', cardLoad(initialCards));
